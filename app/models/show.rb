7cc45cf16b26f1853fa981e192e3733c4c1c00de
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-13 22:38:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    self.actors.map{|actor| actor.first_name actor.last_name}
+    self.actors.map{|actor| "#{actor.first_name} #{actor.last_name}"}
   end
 end
